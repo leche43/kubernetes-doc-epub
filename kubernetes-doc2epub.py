d46f44864d5c6248fd6bbdedf34f144435192f6b
Thomas Gonzalez <leche43@gmx.de>
2019-06-16 23:21:27 +0200

    added metadata and removed unused docs

--- kubernetes-doc2epub.py
+++ kubernetes-doc2epub.py
@@ -1,6 +1,16 @@
 import requests_html as rh
 import pypandoc
 import sys
+import os
+
+directories = [\
+                   "Setup",
+                   "Concepts",
+                   "Tasks",
+                   "Tutorials",
+                   "Reference",
+                   "Contribute"
+                   ]
 
 def generate_directory_epub(url, name, s=None):
     s = rh.HTMLSession() if not s else s
@@ -11,6 +21,9 @@ def generate_directory_epub(url, name, s=None):
     links = [a.absolute_links.pop() for a in anchors]
     # links = filter(lambda href: href.find('/scratch/') == -1, links) # filter out links
 
+    if not os.path.exists('./tmp/'):
+        os.makedirs('./tmp/')
+
     print("downloading...")
     for l1 in links:
         r2 = s.get(l1)
@@ -20,34 +33,24 @@ def generate_directory_epub(url, name, s=None):
             try:
                 if name in ["Setup", "Tutorials", "Reference"]:  # will give duplicate id error, go through pages one by one to skip error page
                     print("testing " + l1, end='')
-                    output = pypandoc.convert_text(div.html, "epub3", format="html", outputfile="/kubernetes_epub_doc_tmp.epub", extra_args=[])
+                    output = pypandoc.convert_text(div.html, "epub3", format="html", outputfile="./tmp/kubernetes_epub_doc_tmp.epub", extra_args=[])
                     print(" works")
             except Exception as e:
                 print(" failed!")
                 print(e)
             else:
                 html += div.html
-        with open("/{}.html".format(name), "wt", encoding="utf-8") as f:
+        with open("./tmp/{}.html".format(name), "wt", encoding="utf-8") as f:
             f.write(html)
 
     print("generating epub for " + name)
     try:
-        output = pypandoc.convert_text(html, "epub3", format="html", outputfile="./{}.epub".format(name), extra_args=['--css=codeblock_wrap.css'])
+        output = pypandoc.convert_text(html, "epub3", format="html", outputfile="./{}.epub".format(name), extra_args=['--css=codeblock_wrap.css', '--epub-metadata=./metadata.xml'])
     except Exception as e:
         print(e)
 
 if __name__ == '__main__':
     s = rh.HTMLSession()
-    directories = [\
-                   "Setup",
-                   "Concepts",
-                   "Getting-started-guides",
-                   "Tasks",
-                   "Tutorials",
-                   "Reference",
-                   # "Admin",  # this direct to concepts
-                   "Imported",
-                   ]
     directories_pairs = [("https://kubernetes.io/docs/{}/".format(n.lower()), n) for n in directories]
     for url, name in directories_pairs:
         print(name)
